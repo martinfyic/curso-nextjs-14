c771c5579a2ec12e0a7c7d818da3fae7f25eb50c
Martin Ferreira <martin.f.yic@gmail.com>
2024-01-11 22:42:34 -0300

    fix: :sparkles: add favorite button functionaliti

--- src/pokemons/components/PokemonCard.tsx
+++ src/pokemons/components/PokemonCard.tsx
@@ -1,8 +1,12 @@
+'use client';
+
 import Link from 'next/link';
 import Image from 'next/image';
 
 import { SimplePokemon } from '..';
-import { IoHeartOutline } from 'react-icons/io5';
+import { IoHeart, IoHeartOutline } from 'react-icons/io5';
+import { useAppDispatch, useAppSelector } from '@/store';
+import { toggleFavorite } from '@/store/pokemons/pokemonsSlice';
 
 interface Props {
 	pokemon: SimplePokemon;
@@ -10,6 +14,12 @@ interface Props {
 
 export const PokemonCard = ({ pokemon }: Props) => {
 	const { id, name } = pokemon;
+	const isFavorite = useAppSelector(state => !!state.pokemons[id]);
+	const dispatch = useAppDispatch();
+
+	const handleToggle = () => {
+		dispatch(toggleFavorite(pokemon));
+	};
 
 	return (
 		<div className='mx-auto right-0 mt-2 w-60'>
@@ -34,20 +44,20 @@ export const PokemonCard = ({ pokemon }: Props) => {
 						</Link>
 					</div>
 				</div>
-				<div className='border-b'>
-					<Link
-						href='/dashboard/main'
-						className='px-4 py-2 hover:bg-gray-100 flex justify-end items-center'
+				<div className='border-b flex flex-row justify-between items-center px-4'>
+					<div className='pr-3'>
+						<p className='text-sm font-medium text-gray-600 leading-none'>
+							# {id.padStart(3, '0')}
+						</p>
+					</div>
+					<button
+						onClick={handleToggle}
+						className='px-4 py-2 hover:bg-gray-100 flex justify-end items-center cursor-pointer'
 					>
-						<div className='pr-3'>
-							<p className='text-sm font-medium text-gray-600 leading-none'>
-								Favorito
-							</p>
+						<div className='text-red-600'>
+							{isFavorite ? <IoHeart /> : <IoHeartOutline />}
 						</div>
-						<div className='text-red-400'>
-							<IoHeartOutline />
-						</div>
-					</Link>
+					</button>
 				</div>
 			</div>
 		</div>
